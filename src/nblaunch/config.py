659e2a9c6e7d28b1a32c4cfa27bc9a1ef04cc59f
Julien Chastang <julien.c.chastang@gmail.com>
2026-03-07 01:19:25 +0000

    static analysis

--- src/nblaunch/config.py
+++ src/nblaunch/config.py
@@ -2,10 +2,10 @@
 
 from __future__ import annotations
 
+from collections.abc import Mapping
 from dataclasses import dataclass
 from os import environ as os_environ
 from os.path import isabs
-from typing import Mapping
 from urllib.parse import urlparse
 
 
